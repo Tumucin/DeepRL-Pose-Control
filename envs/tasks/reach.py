f5864e4814d5f2a03442762f67fbbb7535288530
Tumucin <baltumucin34@gmail.com>
2023-03-15 19:32:24 +0300

    ok

---
+++ envs/tasks/reach.py
@@ -0,0 +1,163 @@
+from typing import Any, Dict, Union
+import numpy as np
+import math
+from panda_gym.envs.core import Task
+from panda_gym.utils import distance
+from yaml.loader import SafeLoader
+import yaml
+#from ..panda_tasks.kinematics import KINEMATICS
+#import PyKDL
+import PyKDL
+from ..utils.kinematics import KINEMATICS
+    
+#import PyKDL
+class Reach(Task):
+    def __init__(
+        self,
+        sim,
+        get_ee_position,
+        reward_type="sparse",
+        distance_threshold=0.01,
+        goal_range=0.3,
+        
+    ) -> None:
+        super().__init__(sim)
+        
+       
+        self.kinematics = KINEMATICS('/home/tumu/anaconda3/envs/stableBaselines/panda-gym/panda_gym/envs/robots/panda.urdf')
+        self.reward_type = reward_type
+        self.distance_threshold = distance_threshold
+        self.get_ee_position = get_ee_position
+        self.goal_range_low = np.array([-goal_range / 2, -goal_range / 2, 0])
+        self.jointLimitLow = np.array([-math.pi, 0, -2.9, -math.pi, -2.9, 0, -2.9])
+        #self.jointLimitLow = np.array([0, math.pi/4-0.3, 0.00, 0.00, 0.00, 0, 0.00])
+        self.jointLimitHigh = np.array([math.pi, math.pi/2, 2.9, 0.00, 2.9, 3.8, 2.9])
+        #self.jointLimitHigh = np.array([0, math.pi/4+0.3, 0.00, 0.00, 0.00, 0, 0.00])
+        self.goal_range_high = np.array([goal_range / 2, goal_range / 2, goal_range])
+            
+        
+
+        with self.sim.no_rendering():
+            self._create_scene()
+            self.sim.place_visualizer(target_position=np.zeros(3), distance=0.9, yaw=45, pitch=-30)
+        self.previousJointVelocities = 0
+
+    def _create_scene(self) -> None:
+        self.sim.create_plane(z_offset=-0.4)
+        self.sim.create_table(length=1.1, width=0.7, height=0.4, x_offset=-0.3)
+        self.sim.create_sphere(
+            body_name="target",
+            radius=0.05,
+            mass=0.0,
+            ghost=True,
+            position=np.zeros(3),
+            rgba_color=np.array([0.1, 0.9, 0.1, 0.3]),
+        )
+
+    def get_obs(self) -> np.ndarray:
+        return np.array([])  # no tasak-specific observation
+
+    def get_achieved_goal(self) -> np.ndarray:
+        ee_position = np.array(self.get_ee_position())
+        return ee_position
+
+    def reset(self) -> None:
+        #print("reset in reach.py")
+        self.goal = self._sample_goal()
+        self.sim.set_base_pose('target', self.goal, np.array([0.0, 0.0, 0.0, 1.0]))
+
+    def _sample_goal(self) -> np.ndarray:
+        """Randomize goal."""
+        goal = self.np_random.uniform(self.goal_range_low, self.goal_range_high)
+        sampledAngles = self.np_random.uniform(self.jointLimitLow, self.jointLimitHigh)
+        #print("sampledAngles:", sampledAngles)
+        q_in = PyKDL.JntArray(self.kinematics.numbOfJoints)
+        
+        q_in[0], q_in[1], q_in[2], q_in[3] =sampledAngles[0], sampledAngles[1], sampledAngles[2], sampledAngles[3]
+        q_in[4], q_in[5], q_in[6] = sampledAngles[4], sampledAngles[5], sampledAngles[6]
+        #print("q_in:", q_in)
+        goalFrame = self.kinematics.forwardKinematicsPoseSolv(q_in)
+        goalFrame.p[0] = goalFrame.p[0] -0.6
+        #print("goal frame:", goalFrame.p)
+        goal[0], goal[1], goal[2] = goalFrame.p[0], goalFrame.p[1], goalFrame.p[2]
+        #goal = np.array([0.0,0.00,0.00])
+        #print("goal in reach.py:", goal)
+        return goal
+
+    def is_success(self, achieved_goal: np.ndarray, desired_goal: np.ndarray) -> Union[np.ndarray, float]:
+        d = distance(achieved_goal, desired_goal)
+        return np.array(d < self.distance_threshold, dtype=np.float64)
+
+    def compute_reward(self,achieved_goal,desired_goal, info: Dict[str, Any]) -> Union[np.ndarray, float]:
+        #achieved_goal = obs['achieved_goal']
+        
+        d = distance(achieved_goal, desired_goal)
+        orientation = self.sim.get_link_orientation('panda', 0)
+        #print("orientation in reach.py:", orientation)
+        #networkAction = obs['observation'][14:20]
+        #print("network action in reach.py", networkAction)
+        currentJointVelocities = np.array([self.sim.get_joint_velocity("panda",joint=i) for i in range(7)])
+        #print("currentJointVelocities:",currentJointVelocities)
+        """
+        try:
+            #print("d shape in reach.py:", len(d))
+            #print("d:", d)
+            #print("desired goal shape in reach.py:", len(desired_goal))
+            #print("desired goal in reach.py:", desired_goal)
+            #print("achieved goal shape in reach.py:", len(achieved_goal))
+            #print("achieved goal in reach.py:", achieved_goal)
+            #print("d:", d)
+            #print("velocity norm:",np.linalg.norm(currentJointVelocities))
+            #mask = d < 0.05
+            #print("mask:", mask)
+            #print(-np.array(np.linalg.norm(currentJointVelocities) > 0.02, dtype=np.float64))
+            #print("velocitty reward:", -(mask)*np.array(np.linalg.norm(currentJointVelocities) > 0.02, dtype=np.float64))
+            pass
+        except TypeError:
+            #print(d)
+            pass
+        """
+        #print("achievend goal in reach.py", achieved_goal)
+        #print("desired_goal goal in reach.py", desired_goal)
+        
+        #print("current joint vel norm in reach.py:", np.linalg.norm(currentJointVelocities))
+        get_joint_angle = np.array([self.sim.get_joint_angle("panda",joint=i) for i in range(7)])
+        #print("get_joint_angle: in reach.py",get_joint_angle)
+        #print("previousJointVelocities:",self.previousJointVelocities)
+        currentJointAccelerations = (currentJointVelocities - self.previousJointVelocities)/(self.sim.timestep)
+        #print("currentJointAccelerations:",currentJointAccelerations)
+        self.previousJointVelocities = currentJointVelocities
+        lambdaErr = 100.0
+        accelerationConstant = 0.0
+        velocityConst = 7.0
+        velocityThreshold = 0.08
+        #print("d in reach.py:", d)
+        #print("currentJointAccelerations norm in reach.py:", np.linalg.norm(currentJointAccelerations))
+        #print("acceleration norm:", 0.0075*np.linalg.norm(currentJointAccelerations))
+        #print("velocities norm:", 0.005*np.linalg.norm(currentJointVelocities))
+        #print("d in reach.py", d)
+        #print("d in reach.py:",d)
+        #print("-d:",-d)
+        #print("second term:", np.array(d < self.distance_threshold, dtype=np.float64))
+        #print("third term:", np.array(np.linalg.norm(currentJointVelocities) < 0.02, dtype=np.float64))
+        #print("real third term:", np.array(d < self.distance_threshold, dtype=np.float64)*np.array(np.linalg.norm(currentJointVelocities) < 0.02, dtype=np.float64))
+        #print("sum:", -d +np.array(d < self.distance_threshold, dtype=np.float64)+np.array(d < self.distance_threshold, dtype=np.float64)*np.array(np.linalg.norm(currentJointVelocities) < 0.02, dtype=np.float64))
+        #print("joint velocities norm:", np.linalg.norm(currentJointVelocities))
+        #print(np.array(d < self.distance_threshold, dtype=np.float64)*np.array(np.linalg.norm(currentJointVelocities) < 0.2, dtype=np.float64) )
+        #print("reward:", 10*np.array(d < self.distance_threshold, dtype=np.float64)*np.array(np.linalg.norm(currentJointVelocities) < 0.6, dtype=np.float64) )
+        if self.reward_type == "sparse":
+            #if type(d)=='float' and d > 0.005:
+            #    return np.exp(-(lambdaErr)*(d*d)) - accelerationConstant*np.linalg.norm(currentJointAccelerations)
+            #else:
+            #    return np.exp(-(lambdaErr)*(d*d)) - accelerationConstant*np.linalg.norm(currentJointAccelerations) - velocityConst*np.linalg.norm(currentJointVelocities)
+            return np.exp(-(lambdaErr)*(d*d)) - accelerationConstant*np.linalg.norm(currentJointVelocities)
+            #return -np.array(d > self.distance_threshold, dtype=np.float64) #-np.array(np.linalg.norm(currentJointVelocities) > 0.02, dtype=np.float64)
+            #return -np.array(np.linalg.norm(currentJointVelocities) > 0.02, dtype=np.float64)
+            #return np.exp(-(lambdaErr)*(d*d)) - accelerationConstant*np.linalg.norm(currentJointAccelerations)
+        else:
+            #return -d + \
+             #       np.array(d < self.distance_threshold, dtype=np.float64)*np.array(np.linalg.norm(currentJointVelocities) < 0.2, dtype=np.float64) 
+                    #np.array(d < self.distance_threshold, dtype=np.float64) + \
+            return np.exp(-(lambdaErr)*(d*d)) - accelerationConstant*np.linalg.norm(currentJointAccelerations) - (velocityConst*np.linalg.norm(currentJointVelocities))/(1+d)+ \
+                   2*np.array(d < self.distance_threshold, dtype=np.float64)*np.array(np.linalg.norm(currentJointVelocities) < velocityThreshold, dtype=np.float64)     
+            #return -np.array(d > self.distance_threshold, dtype=np.float64)   
---
+++ envs/trainTest.py
@@ -0,0 +1,130 @@
+import gym
+#import gymnasium as gym
+import panda_gym
+from stable_baselines3 import PPO, DDPG, HerReplayBuffer
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.callbacks import CheckpointCallback
+import argparse
+import os 
+import yaml
+from yaml.loader import SafeLoader
+from sb3_contrib import TQC
+import time
+import numpy as np
+import torch as th
+
+
+def train(config, algorithm,env):
+    checkpoint_callback = CheckpointCallback(save_freq=config['save_freq'], save_path=config['modelSavePath'],
+                                         name_prefix='PandaReach-v2TQC_'+config['expNumber'])
+    modelDir = config['modelSavePath']
+    logDir = config['logSavePath']
+    if config['algorithm']=="PPO":
+        policy_kwargs = dict(activation_fn=th.nn.ReLU, net_arch=[dict(pi=[128, 128], vf=[128,128,])])
+
+        model = algorithm(policy=config['policy'], env=env, n_steps=config['n_steps'],verbose=1, batch_size=config['batch_size'], learning_rate=config['learning_rate'],
+                          tensorboard_log=logDir+"/"+config['envName']+"_"+config['algorithm'],policy_kwargs=policy_kwargs)
+    
+    if config['algorithm']=="DDPG":
+        model = algorithm(policy=config['policy'], env=env, replay_buffer_class=HerReplayBuffer, verbose=1, 
+             gamma=config['gamma'], batch_size=config['batch_size'], buffer_size=config['buffer_size'], learning_rate = 1e-3, replay_buffer_kwargs = config['rb_kwargs'],
+             policy_kwargs = config['policy_kwargs'], tensorboard_log=logDir+"/"+config['envName']+""+config['algorithm'])
+    
+    if config['algorithm']=="TQC":
+        #c = dict(activation_fn=th.nn.Tanh, net_arch=[dict(pi=[128, 128,128], vf=[128,128, 128])])
+        #policy_kwargs = dict(activation_fn=th.nn.LeakyReLU,net_arch=dict(pi=[128, 128], qf=[128, 128]))
+        if config['continueTraining'] ==True:
+            #model = algorithm.load(modelDir+"/"+config['envName']+""+config['algorithm']+"_"+config['expNumber'], env=env)
+            #model.set_env(env)
+            pass
+        else:
+            #print("here")
+            model = algorithm(policy=config['policy'], env=env, tensorboard_log=logDir+"/"+config['envName']+""+config['algorithm'],
+                            verbose=1, ent_coef=config['ent_coef'], batch_size=config['batch_size'], gamma=config['gamma'],
+                            learning_rate=config['learning_rate'], learning_starts=config['learning_starts'],replay_buffer_class=HerReplayBuffer,
+                            replay_buffer_kwargs=config['replay_buffer_kwargs'], policy_kwargs=config['policy_kwargs'])
+    model.learn(total_timesteps=config['total_timesteps'], callback=checkpoint_callback)
+    model.save(modelDir+"/"+config['envName']+""+config['algorithm']+"_"+config['expNumber'])     
+
+    del model
+
+def load_model(parentDir,config,steps, algorithm,env):
+    modelDir = config['modelSavePath']
+    model = algorithm.load(modelDir+"/"+config['envName']+""+config['algorithm']+"_"+config['expNumber'], env=env) 
+    env = model.get_env()
+    mae = 0.0
+    squaredError = 0.0
+    successRate1 = 0.0
+    successRate5 = 0.0
+    avgJntVel = 0.0
+    for step in range(steps):
+        print("step:", step)
+        counter = 0        
+        done = False
+        obs = env.reset()
+        episode_reward = 0.0
+        while not done:
+            counter+=1
+            action, _states = model.predict(obs, deterministic=True)
+            obs, reward, done, info = env.step(action)
+            #print("obs['achieved_goal']",obs['achieved_goal'])
+            #print("obs['desired_goal']",obs['desired_goal'])
+            if counter==499:
+                error = abs(obs['achieved_goal'] - obs['desired_goal'])
+                #print("error:", error)
+                #print("errorMagnitude:", np.linalg.norm(error))
+                #print("obs['achieved_goal']",obs['achieved_goal'])
+                #print("obs['desired_goal']",obs['desired_goal'])
+                mae = np.linalg.norm(error) + mae
+                squaredError += np.sum(error**2)
+                avgJntVel = np.linalg.norm(action) + avgJntVel
+                if np.linalg.norm(error) <=0.01:
+                    successRate1+=1
+                
+                if np.linalg.norm(error) <=0.05:
+                    successRate5+=1
+            if done:
+                #print("episode done.")
+                pass
+
+            episode_reward+=reward
+            #env.render()
+        #print("episode reward is:", episode_reward)
+    #print("Squared Error:",squaredError)
+    print("RMSE:", np.sqrt((squaredError)/(steps)))
+    print("MAE:", mae/steps)
+    print("Success Rate 1 cm:", successRate1/steps)
+    print("Success Rate 5 cm:", successRate5/steps)
+    print("Average joint velocities:", avgJntVel/steps)
+        
+        
+def main():
+
+    with open('configPPO.yaml') as f:
+        config = yaml.load(f, Loader=SafeLoader)
+
+    currentDir = os.getcwd()
+    if config['algorithm']=="PPO":
+        algorithm = PPO
+        
+    if config['algorithm']=="DDPG":
+        algorithm = DDPG
+
+    if config['algorithm']=="TQC":
+        algorithm = TQC
+    
+    env = gym.make(config['envName'], render=config['render'])
+    
+    env._max_episode_steps = 700
+    
+    if config['mode'] == True:
+        env = make_vec_env('PandaReach-v2', n_envs=4)
+        start_time = time.time()
+        train(config,algorithm, env)
+        print("Total time:", time.time()-start_time)
+    else:
+        load_model(currentDir,config,100, algorithm,env)
+    
+if __name__=='__main__':
+    main()
