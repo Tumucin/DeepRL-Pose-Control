28c590a50f7be1213004fccce7f63e1ac62c0b4c
tbal21 <tbal21@login03.kuacc.ku.edu.tr>
2023-03-22 10:16:22 +0300

    hpc

--- envs/moveOutput.sh
+++ envs/moveOutput.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {61..69}
+for i in {70..101}
 do
 	mv /kuacc/users/tbal21/.conda/envs/stableBaselines/panda-gym/panda_gym/envs/PPOexp${i}.out /kuacc/users/tbal21/.conda/envs/stableBaselines/panda-gym/panda_gym/envs/shOutput/ 
 done
